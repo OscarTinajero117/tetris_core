63c30cf35f6afd8816fc4e07e29aec9e341927ad
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-17 15:35:41 -0600

    init

---
+++ lib/tetris_core.ex
@@ -0,0 +1,18 @@
+defmodule TetrisCore do
+  @moduledoc """
+  Documentation for `TetrisCore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TetrisCore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

ce9b4ce2f17030d4ea55635df2f57570e7788d25
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-17 15:52:40 -0600

    remove comments

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -5,25 +5,19 @@ defmodule BrickTest do
 
   doctest TetrisCore
 
-  # test "Creates a new brick" do
-  #   assert new_brick().name == :i
-  # end
-
   describe "Creates a random brick" do
     test "Check a random name brick" do
-      assert new_random().name in ~w(i l z o t)a
+      assert new_random().name in name_list()
     end
 
     test "Check a rotation brick" do
-      assert new_random().rotation in [0, 90, 180, 270]
+      assert new_random().rotation in rotation_list()
     end
 
     test "Check a reflection brick" do
-      assert new_random().reflection in [true, false]
+      assert new_random().reflection in reflection_list()
     end
 
   end
 
-
-  # def new_brick, do: new()
 end

3c6bd5217452be5561b8df05fdf4c059b247405d
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-18 16:53:14 -0600

    format and change name

--- lib/points.ex
+++ lib/points.ex
@@ -1,5 +1,5 @@
 defmodule Tetris.Points do
-  def translate(points, {x, y}) do
+  def move_to_location(points, {x, y} = _location) do
     Enum.map(points, fn {dx, dy} -> {x + dx, y + dy} end)
   end
 
@@ -13,8 +13,8 @@ defmodule Tetris.Points do
     |> Enum.map(fn {x, y} -> {5 - x, y} end)
   end
 
-  def mirror(points,false), do: points
-  def mirror(points,true), do: mirror(points)
+  def mirror(points, false), do: points
+  def mirror(points, true), do: mirror(points)
 
   def flip(points) do
     points
@@ -29,6 +29,13 @@ defmodule Tetris.Points do
     |> transpose
     |> mirror
   end
+
+  def with_color(points, color) do
+    Enum.map(points, fn point -> add_color(point, color) end)
+  end
+
+  defp add_color({_x, _y, _c} = point, _color), do: point
+  defp add_color({x, y}, color), do: {x, y, color}
 
   def to_string(points) do
     map =

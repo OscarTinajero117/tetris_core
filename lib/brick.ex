afa328440c1c3d7022cdc586652c58a07c3fd931
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-18 13:38:08 -0600

    reacomodo

--- lib/brick.ex
+++ lib/brick.ex
@@ -2,12 +2,10 @@ defmodule Tetris.Brick do
   @moduledoc """
   # Documentation for `Tetris.Brick`.
   """
-  defstruct [
-    name: :i,
-    location: {40, 0},
-    rotation: 0,
-    reflection: false
-  ]
+  defstruct name: :i,
+            location: {40, 0},
+            rotation: 0,
+            reflection: false
 
   @doc """
   ## Tetris.Brick.new_random.
@@ -30,7 +28,7 @@ defmodule Tetris.Brick do
       location: {40, 0},
       rotation: random_rotation(),
       reflection: random_reflection()
-      }
+    }
   end
 
   @doc """
@@ -73,50 +71,80 @@ defmodule Tetris.Brick do
     # TODO: Move brick to final position
   end
 
-  def down(brick) do
-    %{brick | location: point_down(brick.location)}
+  def down(brick), do: %{brick | location: point_down(brick.location)}
+
+  def left(brick), do: %{brick | location: point_left(brick.location)}
+
+  def right(brick), do: %{brick | location: point_right(brick.location)}
+
+  def spin_90(brick), do: %{brick | rotation: rotate(brick.rotation)}
+
+  def shape(%{name: :l}) do
+    [
+      {2, 1},
+      {2, 2},
+      {2, 3},
+      {3, 3}
+    ]
   end
 
-  def left(brick) do
-    %{brick | location: point_left(brick.location)}
+  def shape(%{name: :i}) do
+    [
+      {2, 1},
+      {2, 2},
+      {2, 3},
+      {2, 4}
+    ]
   end
 
-  def right(brick) do
-    %{brick | location: point_right(brick.location)}
+  def shape(%{name: :o}) do
+    [
+      {2, 2},
+      {3, 2},
+      {2, 3},
+      {3, 3}
+    ]
   end
 
-  def spin_90(brick) do
-    %{brick | rotation: rotate(brick.rotation)}
+  def shape(%{name: :z}) do
+    [
+      {2, 2},
+      {2, 3},
+      {3, 3},
+      {3, 4}
+    ]
+  end
+
+  def shape(%{name: :t}) do
+    [
+      {2, 1},
+      {2, 2},
+      {3, 2},
+      {2, 3}
+    ]
   end
 
   defp rotate(270), do: 0
-
   defp rotate(degrees), do: degrees + 90
+
+  defp point_down({x, y}), do: {x, y + 1}
+
+  defp point_left({x, y}), do: {x - 1, y}
+
+  defp point_right({x, y}), do: {x + 1, y}
 
   defp random_name() do
     name_list()
-      |> Enum.random
+    |> Enum.random()
   end
 
   defp random_reflection() do
     reflection_list()
-      |> Enum.random
+    |> Enum.random()
   end
 
   defp random_rotation() do
     rotation_list()
-      |> Enum.random
-  end
-
-  defp point_down({x, y}) do
-    {x, y + 1}
-  end
-
-  defp point_left({x, y}) do
-    {x - 1, y}
-  end
-
-  defp point_right({x, y}) do
-    {x + 1, y}
+    |> Enum.random()
   end
 end

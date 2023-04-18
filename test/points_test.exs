927b8b567c752fb364c0d4c367094a3b0bbc789c
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-18 14:45:19 -0600

    se agregan test de puntos

---
+++ test/points_test.exs
@@ -0,0 +1,26 @@
+defmodule PointsTest do
+  use ExUnit.Case
+
+  import Tetris.Points
+
+  doctest TetrisCore
+
+  describe "Check Translate and Rotation" do
+    test "Should flip, rotate and mirror" do
+      [{1, 1}]
+      |> mirror
+      |> assert_points({4, 1})
+      |> flip
+      |> assert_points({4, 4})
+      |> rotate(90)
+      |> assert_points({1, 4})
+      |> rotate_90
+      |> assert_points({1, 1})
+    end
+
+    defp assert_points([points], expected) do
+      assert points == expected
+      [points]
+    end
+  end
+end

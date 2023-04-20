4ff1c5779de463ffd70dd558b3a626bbb997722e
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-20 16:41:30 -0600

    upgrade bottom

--- lib/bottom.ex
+++ lib/bottom.ex
@@ -4,10 +4,11 @@ defmodule Tetris.Bottom do
     |> Enum.map(fn {x, y, c} -> {{x, y}, {x, y, c}} end)
     |> Enum.into(bottom)
   end
-
+  
   def collides?(bottom, {x, y, _color}), do: collides?(bottom, {x, y})
-  def collides?(bottom, {x, y}), do: !!Map.get(bottom, {x, y})
-
+  def collides?(bottom, {x, y}) do
+    !!Map.get(bottom, {x, y}) || x < 1 || x > 10 || y > 20
+  end
   def collides?(bottom, points) when is_list(points) do
     Enum.any?(points, &collides?(bottom, &1))
   end

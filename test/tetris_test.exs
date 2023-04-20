5907b7eab90f0bc5e5a4aafff5748d077d5ecadd
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-20 16:42:06 -0600

    add mechanic of game

--- test/tetris_test.exs
+++ test/tetris_test.exs
@@ -1,8 +1,27 @@
 defmodule TetrisTest do
   use ExUnit.Case
-  doctest Tetris
 
-  test "greets the world" do
-    assert Tetris.hello() == :world
+  import Tetris
+  alias Tetris.{Brick}
+  
+  describe "Get out of size" do
+    test "Try to move right, success" do
+      brick = Brick.debug_brick(location: {5, 1})
+      bottom = %{}
+
+      expected = brick |> Brick.right
+      actual = try_right(brick, bottom)
+
+      assert expected == actual
+    end
+    
+    test "Try to move right, failure (returns origianl brick)" do
+      brick = Brick.debug_brick(location: {8, 1})
+      bottom = %{}
+
+      actual = try_right(brick, bottom)
+
+      assert actual == brick
+    end
   end
 end

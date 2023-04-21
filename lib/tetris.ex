76901097bc5094ac0151d4131acd67ee43b52b16
OscarTinajero117 <tinajero117.oscar@gmail.com>
2023-04-20 18:41:25 -0600

    add drop and merge mechanics and test

--- lib/tetris.ex
+++ lib/tetris.ex
@@ -16,6 +16,34 @@ defmodule Tetris do
       new_brick
     end
   end
+
+  def drop(brick, bottom, color) do
+    new_brick = Brick.down(brick)
+    collided = Bottom.collides?(bottom, prepare(new_brick))
+    
+    maybe_do_drop(collided, bottom, brick, new_brick, color)
+  end
+
+  def maybe_do_drop(true=_collided, bottom, old_brick, _new_brick, color)do
+    points = 
+      old_brick
+      |> prepare
+      |> Points.with_color(color)
+
+    %{
+      brick: Brick.new_random(),
+      bottom: Bottom.merge(bottom, points),
+      score: 100
+    }
+  end
+
+  def maybe_do_drop(false=_collided, bottom, _old_brick, new_brick, _color)do
+    %{
+      brick: new_brick,
+      bottom: bottom,
+      score: 1
+    }
+  end
   
   def try_left(brick, bottom), do: try_move(brick, bottom, &Brick.left/1)
   def try_right(brick, bottom), do: try_move(brick, bottom, &Brick.right/1)
--- test/tetris_test.exs
+++ test/tetris_test.exs
@@ -23,5 +23,28 @@ defmodule TetrisTest do
 
       assert actual == brick
     end
+
+    test "Drops without merging" do
+      brick = Brick.debug_brick(location: {5, 5})
+      bottom = %{}
+
+      expected = %{
+        brick: brick |> Brick.down,
+        bottom: bottom,
+        score: 1
+      }
+      actual = drop(brick, bottom, :red)
+
+      assert expected == actual
+    end
+
+    test "Drops and merges" do
+      brick = Brick.debug_brick(location: {5, 16})
+      bottom = %{}
+
+      actual = drop(brick, bottom, :red)
+
+      assert Map.get(actual.bottom, {7, 20}) == {7, 20, :red}
+    end
   end
 end
